c0c2caf6972a6398ad5d69854b85e41669a5c1d5
Oladimeji Odusote <taiwo.odusote@yahoo.com>
2023-04-05 10:39:32 -0400

    python lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,25 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, name):
+        self.name = name
+
+    def match(self,list_of_words):
+        #print(list_of_words)
+        solutions = []
+        for i in list_of_words:
+            word_list = list(i)
+            word_list.sort()
+            name_list = list(self.name)
+            name_list.sort()
+            if (word_list == name_list):
+                solutions.append(i)
+
+        return solutions
+
+
+    
+
+listen = Anagram("listen")
+print (listen.name)
+print(listen.match(['enlists', 'google', 'inlets', 'banana']))
